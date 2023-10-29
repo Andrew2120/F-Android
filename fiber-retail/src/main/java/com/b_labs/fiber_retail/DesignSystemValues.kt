8281f43c8a37181317d5dce706d7d9cb545bb7db
Andrew2120 <andrew-2120@live.com>
2023-10-29 13:44:06 +0000

    Copy DesignSystem.kt from fiber-core

--- fiber-retail/src/main/java/com/b_labs/fiber_retail/DesignSystemValues.kt
+++ fiber-retail/src/main/java/com/b_labs/fiber_retail/DesignSystemValues.kt
@@ -266,7 +266,7 @@ val designSystem = DesignSystem(
                 semanticDanger0 = Color(parseColor("#c53434"))
             ),
             brand = BrandValuesContainer(
-                semanticBrandNegative4 = Color(parseColor("#f59638")),
+                semanticBrandNegative4 = Color(parseColor("#246626")),
                 semanticBrandNegative3 = Color(parseColor("#113997")),
                 semanticBrandNegative2 = Color(parseColor("#1e4fc2")),
                 semanticBrandNegative1 = Color(parseColor("#2759ce")),
